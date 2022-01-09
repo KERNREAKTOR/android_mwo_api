ddc4200eede41f41d3865436b12817fbcc01b84a
Kernreaktor <mail-an-kernreaktor@web.de>
2022-01-09 12:26:49 +0100

    Version 1.0.0

---
+++ app/src/main/java/com/example/mobilemwoapiapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mobilemwoapiapp;
+
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnstart;
+    TextView tvstarus;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnstart = findViewById(R.id.btn_start);
+        tvstarus = findViewById(R.id.status_text);
+
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
